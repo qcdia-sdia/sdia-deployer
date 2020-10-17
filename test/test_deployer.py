b1799280a292ae632719681fbb852cd6e4cd206b
skoulouzis <S.Koulouzis@uva.nl>
2020-10-17 15:53:47 +0200

    added StrictHostKeyChecking=no

--- test/test_deployer.py
+++ test/test_deployer.py
@@ -31,7 +31,7 @@ class TestDeployer(unittest.TestCase):
         tosca_template_path = self.get_tosca_template_path(parsed_json_message)
         tosca_helper = ToscaHelper(sure_tosca_base_url, tosca_template_path)
         nodes_pairs = tosca_helper.get_deployment_node_pairs()
-        vms = nodes_pairs[0]
+        vms=tosca_helper.get_vms()
 
     def test(self):
         parsed_json_message = self.get_request_message()
@@ -46,12 +46,14 @@ class TestDeployer(unittest.TestCase):
             self.assertIsNotNone(tosca_helper.doc_id)
             nodes_to_deploy = tosca_helper.get_application_nodes()
             self.assertIsNotNone(nodes_to_deploy)
-            nodes_pairs = tosca_helper.get_deployment_node_pairs()
+            nodes_pairs = tosca_helper.get_deployment_node_pipeline()
             self.assertIsNotNone(nodes_pairs)
 
             username = 'admin'
-            deployService = DeployService(polemarch_base_url=polemarch_base_url,polemarch_username=username,polemarch_password='admin',
-                                          semaphore_base_url=semaphore_base_url,semaphore_username=username,semaphore_password='password')
+            deployService = DeployService(polemarch_base_url=polemarch_base_url,polemarch_username=username,
+                                          polemarch_password='admin', semaphore_base_url=semaphore_base_url,
+                                          semaphore_username=username,semaphore_password='password',
+                                          vms=tosca_helper.get_vms())
             for node_pair in nodes_pairs:
                 deployService.deploy(node_pair)
 

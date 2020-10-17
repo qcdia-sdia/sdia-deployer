d84b4643c663b18a4e92a7e9a964828fdd599d87
skoulouzis <S.Koulouzis@uva.nl>
2020-10-17 14:37:20 +0200

    added correct key to get playbooks

--- service/ansible_service.py
+++ service/ansible_service.py
@@ -51,7 +51,7 @@ class AnsibleService:
                 create = standard['create']
                 inputs = create['inputs']
                 git_url = inputs['repository']
-                playbook_names = inputs['playbooks']
+                playbook_names = inputs['resources']
                 for playbook_name in playbook_names:
                     task_id = self.run_task(name, project_id, key_id, git_url, inventory_id, playbook_name)
                     if self.semaphore_helper.get_task(project_id, task_id).status != 'success':
@@ -61,7 +61,7 @@ class AnsibleService:
                     configure = standard['configure']
                     inputs = configure['inputs']
                     git_url = inputs['repository']
-                    playbook_names = inputs['playbooks']
+                    playbook_names = inputs['resources']
                     for playbook_name in playbook_names:
                         task_id = self.run_task(name, project_id, key_id, git_url, inventory_id, playbook_name)
                         if self.semaphore_helper.get_task(project_id, task_id).status != 'success':

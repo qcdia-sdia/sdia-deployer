05e230752440fb76269b454d9d06cb55e5a31bb4
skoulouzis <S.Koulouzis@uva.nl>
2020-12-02 13:35:53 +0100

    we'll gradually switch to awx. for now we use both

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN apt update -y && apt upgrade -y
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
-COPY docker-compose/requirements.txt /usr/src/app/
+COPY requirements.txt /usr/src/app/
 
 
 RUN pip3 install --no-cache-dir -r requirements.txt
---
+++ docker-compose/credentials.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "awx",
+        'USER': "awx",
+        'PASSWORD': "awxpass",
+        'HOST': "postgres",
+        'PORT': "5432",
+    }
+}
+
+BROADCAST_WEBSOCKET_SECRET = "R3M1V2I6TUY4MVptRmxQOnNaTy13SHdmNyxwQk5NZExvOUJQRkFDN181RmFDenZTRVFJci1oZDlBVzVCaUVkbS4wQ2I4QUdGQWk6S1hMdEgtZGx4U25DaTBvLmsxekNZUURROGN3MGtZOjFVa1NsVXAxTUJyRFI5T3JvZXYuTCw="

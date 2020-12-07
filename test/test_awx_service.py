da09f63fcfd3987f8aaf6e7fd08320dd672ff00d
skoulouzis <S.Koulouzis@uva.nl>
2020-12-07 13:41:56 +0100

    update request message

--- test/test_awx_service.py
+++ test/test_awx_service.py
@@ -37,7 +37,8 @@ awx_password = 'password'
 class TestAWXService(unittest.TestCase):
 
     def test(self):
-        parsed_json_message = self.get_request_message_from_url('https://raw.githubusercontent.com/qcdis-sdia/sdia-deployer/develop/sample_requests/provision_request_workflow.json')
+        # parsed_json_message = self.get_request_message_from_url('https://raw.githubusercontent.com/qcdis-sdia/sdia-deployer/develop/sample_requests/provision_request_workflow.json')
+        parsed_json_message = self.get_request_message_from_file('../sample_requests/provision_request_workflow.json')
         owner = parsed_json_message['owner']
         tosca_file_name = 'tosca_template'
         tosca_template_dict = parsed_json_message['toscaTemplate']
@@ -46,9 +47,9 @@ class TestAWXService(unittest.TestCase):
         tosca_template_path = self.get_tosca_template_path(parsed_json_message)
         tosca_service_is_up = ToscaHelper.service_is_up(sure_tosca_base_url)
         if tosca_service_is_up:
-            tosca_helper = ToscaHelper(sure_tosca_base_url, tosca_template_path)
-            vms = tosca_helper.get_vms()
-            workflows = tosca_helper.get_workflows()
+            # tosca_helper = ToscaHelper(sure_tosca_base_url, tosca_template_path)
+            # vms = tosca_helper.get_vms()
+            # workflows = tosca_helper.get_workflows()
             awx = AWXService(api_url=awx_api,username=awx_username,password=awx_password)
             for tosca_node_name in node_templates:
                 tosca_node = node_templates[tosca_node_name]
@@ -72,8 +73,8 @@ class TestAWXService(unittest.TestCase):
         return parsed_json_message
 
     def get_request_message_from_file(self,path):
-        with open(path, 'w') as outfile:
-            return yaml.dump(path, outfile, default_flow_style=False)
+        with open(path) as json_file:
+            return json.load(json_file)
 
     def get_tosca_template_path(self,parsed_json_message):
         tosca_file_name = 'tosca_template'

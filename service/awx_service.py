fa4a398d374a516ab959b2c37efad8ba476eea4c
skoulouzis <S.Koulouzis@uva.nl>
2020-12-16 00:12:55 +0100

    added 204 'save' code

--- service/awx_service.py
+++ service/awx_service.py
@@ -116,8 +116,10 @@ class AWXService:
         r = self._session.post(self.api_url + '/' + api_path + '/', data=json.dumps(body), headers=self.headers)
         ids = []
         if r.status_code == 201 or r.status_code == 202 or r.status_code == 204:
-            id = r.json()['id']
-            ids.append(id)
+            json_resp = r.json()
+            if 'id' in json_resp:
+                id = json_resp['id']
+                ids.append(id)
             return ids
         elif r.status_code == 400 and 'already exists' in r.text:
             if 'name' in body:
@@ -397,6 +399,8 @@ class AWXService:
                     'identifier': child
                 }
                 child_ids = self.post(body,path)
+                if child_ids:
+                    child_id = child_ids[0]
                 print('-----------------------------')
                 print('parent: '+str(call_operation))
                 label = None
@@ -409,18 +413,18 @@ class AWXService:
                     print(' children: '+str(children))
                     if isinstance(children, list) and child_ids:
                         for child in children:
-                            self.create_workflow_nodes(parent_id=child_ids[0],
+                            self.create_workflow_nodes(parent_id=child_id,
                                                        child=child,
                                                        label=label,
                                                        steps=steps,
                                                        topology_template_workflow_steps=topology_template_workflow_steps)
                     if isinstance(children, str) and child_ids:
                         child = children
-                        self.create_workflow_nodes(parent_id=child_ids[0],
-                                                                       child=child,
-                                                                       label=label,
-                                                                       steps=steps,
-                                                                       topology_template_workflow_steps=topology_template_workflow_steps)
+                        self.create_workflow_nodes(parent_id=child_id,
+                                                   child=child,
+                                                   label=label,
+                                                   steps=steps,
+                                                   topology_template_workflow_steps=topology_template_workflow_steps)
         return None
 
 

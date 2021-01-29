4aba62eca260b8d5a07785b8e7e00a820ae2b766
skoulouzis <S.Koulouzis@uva.nl>
2021-01-29 16:01:54 +0100

    run deployment tests on docker 'vms'

--- test/test_deployer.py
+++ test/test_deployer.py
@@ -1,3 +1,4 @@
+import configparser
 import copy
 import json
 import logging
@@ -6,23 +7,24 @@ import os.path
 import tempfile
 import time
 import urllib
+import traceback
 
 import yaml
-import re  # noqa: F401
-from pathlib import Path
+import re
 import unittest
 
-import sure_tosca_client
-from sure_tosca_client import Configuration, ApiClient
-from sure_tosca_client.api import default_api
-
+from service.awx_service import AWXService
 from service.deploy_service import DeployService
 from service.tosca_helper import  ToscaHelper
 
 sure_tosca_base_url = 'http://localhost:8081/tosca-sure/1.0.0'
-polemarch_base_url = None#'http://localhost:30001/api/v2'
 semaphore_base_url = 'http://localhost:3000/api'
-
+semaphore_password = 'password'
+semaphore_username = 'admin'
+awx_base_url = 'http://localhost:8052/api/v2'
+awx_username = 'admin'
+awx_password = 'password'
+logger = logging.getLogger(__name__)
 
 class TestDeployer(unittest.TestCase):
 
@@ -36,7 +38,7 @@ class TestDeployer(unittest.TestCase):
             nodes_pairs = tosca_helper.get_deployment_node_pipeline()
             vms=tosca_helper.get_vms()
 
-    def test(self):
+    def test_deploy_service(self):
         parsed_json_message = self.get_request_message('https://raw.githubusercontent.com/qcdis-sdia/sdia-deployer/master/sample_requests/deploy_request.json')
         tosca_template_path = self.get_tosca_template_path(parsed_json_message)
         # owner = parsed_json_message['owner']
@@ -53,14 +55,28 @@ class TestDeployer(unittest.TestCase):
             self.assertIsNotNone(nodes_pairs)
 
             username = 'admin'
-            deployService = DeployService(polemarch_base_url=polemarch_base_url,polemarch_username=username,
-                                          polemarch_password='admin', semaphore_base_url=semaphore_base_url,
+            deployService = DeployService(semaphore_base_url=semaphore_base_url,
                                           semaphore_username=username,semaphore_password='password',
                                           vms=tosca_helper.get_vms())
             for node_pair in nodes_pairs:
                 deployService.deploy(node_pair)
 
+    def test_deployer(self):
 
+        tosca_service_is_up = ToscaHelper.service_is_up(sure_tosca_base_url)
+        semaphore_is_up = ToscaHelper.service_is_up(semaphore_base_url)
+
+        if tosca_service_is_up and semaphore_is_up:
+
+
+            f = open('../sample_requests/deploy_request.json', )
+            parsed_json_message = json.load(f)
+            tosca_template_path = self.get_tosca_template_path(parsed_json_message)
+            tosca_template_dict = parsed_json_message['toscaTemplate']
+            if 'workflows' in tosca_template_dict['topology_template']:
+                return self.awx(tosca_template_dict=tosca_template_dict, tosca_template_path=tosca_template_path)
+
+            return self.semaphore(tosca_template_dict=tosca_template_dict, tosca_template_path=tosca_template_path)
 
 
 
@@ -80,6 +96,69 @@ class TestDeployer(unittest.TestCase):
         with open(tosca_template_path, 'w') as outfile:
             yaml.dump(tosca_template_dict, outfile, default_flow_style=False)
         return tosca_template_path
+
+    def semaphore(self,tosca_template_path=None, tosca_template_dict=None):
+        tosca_helper = ToscaHelper(sure_tosca_base_url, tosca_template_path)
+        nodes = tosca_helper.get_application_nodes()
+        # nodes = tosca_helper.get_deployment_node_pipeline()
+
+        deployService = DeployService(semaphore_base_url=semaphore_base_url, semaphore_username=semaphore_username,
+                                      semaphore_password=semaphore_password, vms=tosca_helper.get_vms())
+        try:
+            for node in nodes:
+                updated_node = deployService.deploy(node)
+                if isinstance(updated_node, list):
+                    for node in updated_node:
+                        tosca_template_dict = tosca_helper.set_node(node, tosca_template_dict)
+                        # logger.info("tosca_template_dict :" + json.dumps(tosca_template_dict))
+                else:
+                    tosca_template_dict = tosca_helper.set_node(updated_node, tosca_template_dict)
+                    # logger.info("tosca_template_dict :" + json.dumps(tosca_template_dict))
+
+            response = {'toscaTemplate': tosca_template_dict}
+            output_current_milli_time = int(round(time.time() * 1000))
+            response["creationDate"] = output_current_milli_time
+            logger.info("Returning Deployment")
+            logger.info("Output message:" + json.dumps(response))
+            return json.dumps(response)
+        except Exception as e:
+            track = traceback.format_exc()
+            print(track)
+            raise
+
+
+    def awx(self,tosca_template_path=None, tosca_template_dict=None):
+        tosca_service_is_up = ToscaHelper.service_is_up(sure_tosca_base_url)
+        if tosca_service_is_up:
+            tosca_helper = ToscaHelper(sure_tosca_base_url, tosca_template_path)
+            node_templates = tosca_template_dict['topology_template']['node_templates']
+            awx = AWXService(api_url=awx_base_url, username=awx_username, password=awx_password)
+            topology_template_workflow_steps = {}
+            for tosca_node_name in node_templates:
+                tosca_node = node_templates[tosca_node_name]
+                logger.info('Resolving function values for: '+tosca_node_name)
+                tosca_node = tosca_helper.resolve_function_values(tosca_node)
+                logger.info('Creating workflow steps for: ' + tosca_node_name)
+                node_workflow_steps = awx.create_workflow_steps(tosca_node)
+                topology_template_workflow_steps.update(node_workflow_steps)
+            workflows = tosca_helper.get_workflows()
+            if workflows:
+                launched_ids = []
+                for workflow_name in workflows:
+                    workflow = workflows[workflow_name]
+                    description = None
+                    if 'description' in workflow:
+                        description = workflow['description']
+                    wf_ids = awx.create_workflow(description=description, workflow_name=workflow_name)
+                    logger.info('Created workflow with ID: ' + str(wf_ids[0]))
+                    workflow_node_ids = awx.create_dag(workflow_id=wf_ids[0],
+                                                       tosca_workflow=workflow,
+                                                       topology_template_workflow_steps=topology_template_workflow_steps)
+                    logger.info('Added nodes to workflow')
+                    for wf_id in wf_ids:
+                        wf_job_ids = awx.launch(wf_id)
+                        launched_ids += wf_job_ids
+
 
 
 if __name__ == '__main__':
